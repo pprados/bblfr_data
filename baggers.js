2a2a5fe11f99de020407c10c3b83bc62e244b8eb
Philippe PRADOS <github@prados.fr>
2015-10-14 17:33:08 +0200

    Update baggers.js
    Ajout d'une conf

--- baggers.js
+++ baggers.js
@@ -549,7 +549,7 @@ var data = {
         },
         {
             "name": "Philippe PRADOS",
-            "bio": "Architecte Senior, auteur de plus de 100 articles publiés dans GNU Linux Mag, conférencier à Devoxx, Paris JUG, PAUG, SSTIC, Solution Linux, GDG, DroidCon, etc.",
+            "bio": "Architecte Senior, auteur de plus de 100 articles publiés dans GNU Linux Mag, conférencier à Devoxx, Paris JUG, PAUG, SSTIC, Softshake, Solution Linux, GDG, DroidCon, etc.",
             "picture": "http://www.prados.fr/_/rsrc/1351608832371/cv/MOI%20300x297.png?height=198&width=200",
             "websites": [
                 {
@@ -572,8 +572,20 @@ var data = {
             "twitter": "pprados",
             "contact": "brownbaglunch@prados.fr",
             "mail": "brownbaglunch@prados",
-            "location": "Paris, Banlieue ouest",
+            "location": "Paris",
             "sessions": [
+                {
+                    "title": "Les secrets de la JVM pour les algo à haute fréquence",
+                    "summary": "Découvrez les secrets de la JVM pour permettre la rédaction d'algorithme à haute fréquence. Parcourrons ensemble les quatorze secrets en fouillant dans les sources des librairies et de la JVM."
+                },
+                {
+                    "title": "Le secret de nouvelles architectures: Code Réactif et persistance versionnée.",
+                    "summary": "Comment améliorer les performances de votre SI pour encaisser la multiplication par 10 de vos hits, suite à la publication d'une API pour votre application mobile ? Comment améliorer les modèles actuels d'architecture ? Quels sont les nouveaux patterns de développement ? Pourquoi sont-il maintenant proposé par les langages et frameworks ? Comment les exploiter ? Comment gérer la concurrence, les bases de données avec plus d’efficacité ? Inspirons nous des meilleurs, remettons le développement à l'honneur."
+                },
+                {
+                    "title": "Multithread sans thread",
+                    "summary": "Comment les langages de développement et les compilateurs évoluent pour intégrer l'approche réactive ? Quelles sont les techniques mises en place lors de la génération de codes ? Avec quel impact ? Que savoir pour en tirer parti ? Nous allons comparer les approches de différents langages : Java, Net, Javascript, Scala, Python, etc... Entre le code tel que je l'écris et le code que le compilateur produit, il y a un écart à maîtriser."
+                },
                 {
                     "title": "Architecture Android",
                     "summary": "Comprendre comment est conçu le système d'exploitation mobile de Google. Qu'elles sont les faiblesses et avantages par rapport aux OS alternatifs. Cette conférence s'adresse à tous."
@@ -585,14 +597,6 @@ var data = {
                 {
                     "title": "La mort prochaine du GC ?",
                     "summary": "Des .signaux faibles : Fin de la loi de Moore en 2018. NECESSITE de l’algorithme G1.Ralentissement des programmes en 64 bits par rapport à 32 bits. De nouveaux frameworks pour gérer la mémoire hors heap. Émergence du Big-Data et du Database in Memory. Apple déprécie le GC d’Objective-C. Comment retarder l’échéance ? Les solutions alternatives ? Sélectionnez maintenant les technos de demain."
-                },
-                {
-                    "title": "Le secret de nouvelles architectures: Code Réactif et persistance versionnée.",
-                    "summary": "Comment améliorer les performances de votre SI pour encaisser la multiplication par 10 de vos hits, suite à la publication d'une API pour votre application mobile ? Comment améliorer les modèles actuels d'architecture ? Quels sont les nouveaux patterns de développement ? Pourquoi sont-il maintenant proposé par les langages et frameworks ? Comment les exploiter ? Comment gérer la concurrence, les bases de données avec plus d’efficacité ? Inspirons nous des meilleurs, remettons le développement à l'honneur."
-                },
-                {
-                    "title": "Multithread sans thread",
-                    "summary": "Comment les langages de développement et les compilateurs évoluent pour intégrer l'approche réactive ? Quelles sont les techniques mises en place lors de la génération de codes ? Avec quel impact ? Que savoir pour en tirer parti ? Nous allons comparer les approches de différents langages : Java, Net, Javascript, Scala, Python, etc... Entre le code tel que je l'écris et le code que le compilateur produit, il y a un écart à maîtriser."
                 }
             ],
             "tags": [
@@ -600,7 +604,8 @@ var data = {
                 "Scala",
                 "Java",
                 "Android",
-                "Securité"
+                "Securité",
+                "Reactive","reactif"
             ],
             "cities": [
                 "Paris"
